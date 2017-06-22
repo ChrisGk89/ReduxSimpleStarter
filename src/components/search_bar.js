f445dcdf0d4ad20c19d65157242c299af6f9a0d9
Chris <chris_skart@hotmail.com>
2017-06-22 17:41:01 +0300

    Changes to the search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,14 +5,29 @@ import React, { Component } from 'react';
 //class SearchBar extends React.Component
 
 class SearchBar extends Component {
-  render() {
-    return <input />;
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
   }
 
-  onInputChange() {
-    
+  render() {
+    return (
+      <div>
+        <input 
+          value = {this.state.term}
+          onChange = { event => this.setState ({ term: event.target.value }) } />
+      </div>
+    );
   }
 }
+//We use arrow function instead of simple function-method
+
+/*Method of the class
+  onInputChange(event) {
+    console.log (event.target.value);
+  }
+}*/
 
 
 export default SearchBar;
