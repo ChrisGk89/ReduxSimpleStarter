6864618953de63fa6bc5b1d24e9dada7cf76f26f
Chris <chris_skart@hotmail.com>
2017-06-28 14:57:16 +0300

    New updates.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,18 +8,24 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: '' };
   }
 
   render() {
     return (
-      <div>
-        <input 
+      <div className="search-bar">
+        <input
           value = {this.state.term}
-          onChange = { event => this.setState ({ term: event.target.value }) } />
+          onChange = { event => this.onInputChange(event.target.value )} />
       </div>
     );
   }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 //We use arrow function instead of simple function-method
 
